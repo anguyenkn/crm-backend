19e24184d74d1e0239908637a73c0ee7de67bc4e
Aidan Nguyen <anguyenkn@gmail.com>
2020-06-08 20:39:27 -0700

    added a basic middleware to a route

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,18 +1,30 @@
 const routes = (app) => {
   app
     .route("/contact")
-    .get((req, res) => {
-      res.send("GET request successful!");
-    })
+
+    .get(
+      (req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+      },
+      (req, res, next) => {
+        res.send("GET request successful!");
+      }
+    )
+
     .post((req, res) => {
       res.send("POST request successful!");
     });
 
   app
     .route("/contact/:contactID")
+
     .put((req, res) => {
       res.send("PUT request successful!");
     })
+
     .delete((req, res) => {
       res.send("DELETE request susessful!");
     });
