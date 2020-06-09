a1ccf755cb11e37d6684956ddc7256525a594559
Aidan Nguyen <anguyenkn@gmail.com>
2020-06-08 21:28:32 -0700

    added the get endpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,20 +1,15 @@
-import { addNewContact } from "../controllers/crmController";
+import { addNewContact, getContact } from "../controllers/crmController";
 
 const routes = (app) => {
   app
     .route("/contact")
 
-    .get(
-      (req, res, next) => {
-        // middleware
-        console.log(`Request from: ${req.originalUrl}`);
-        console.log(`Request type: ${req.method}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send("GET request successful!");
-      }
-    )
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContact)
 
     .post(addNewContact);
 
